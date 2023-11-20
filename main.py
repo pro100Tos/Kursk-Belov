c3e0d1f9ae448d6a219a5ddfdb2d5870697c4c19
pro100Tos <118208616+pro100Tos@users.noreply.github.com>
2023-11-20 11:38:23 +0300

    Something_Commit

--- main.py
+++ main.py
@@ -1,14 +1,37 @@
 import sys
 
-from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from PyQt5.QtGui import QPainter,  QColor
 from random import randint
+from PyQt5 import QtCore, QtGui, QtWidgets
 
-class MyWidget(QMainWindow):
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(800, 600)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(80, 90, 91, 41))
+        self.pushButton.setObjectName("pushButton")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.pushButton.setText(_translate("MainWindow", "Рисовать"))
+
+
+class MyWidget(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('UI.ui', self)
+        self.setupUi(self)
         self.initUI()
 
     def initUI(self):
@@ -29,7 +52,7 @@ class MyWidget(QMainWindow):
 
     def draw_fn(self, qp):
         a = randint(30, 200)
-        r, g, b = 255, 255, 0
+        r, g, b = randint(0, 255), randint(0, 255), randint(0, 255)
         qp.setBrush(QColor(r, g, b))
         qp.drawEllipse(300 - a // 2, 400 - a // 2, a, a)
         qp.setBrush(QColor(r, g, b))
